86cefc83e106ca82ba337bc8a722bc57d271f3ce
Rishabh Anand <rishab@caarya.in>
2020-08-27 14:06:31 +0530

    final setup, function arguments setup
     On branch master
     Changes to be committed:
    	modified:   compatibility.js

--- compatibility.js
+++ compatibility.js
@@ -1,4 +1,5 @@
 const constants = require("./constants");
+const { getBirthChart, getNavamsaChart } = require("./position");
 /**
  * @typedef {Object} birthDetails
  * @property {String} dateString - Format: YYYY-MM-DD
@@ -19,6 +20,28 @@ function areCompatibile(
   person1,
   person2,
   threshold = constants.DEFAULT_THRESHOLD
-) {}
+) {
+  const person1BirthChart = getBirthChart(
+    person1.dateString,
+    person1.timeString,
+    person1.lat,
+    person1.lng,
+    person1.timezone
+  );
+  const person2BirthChart = getBirthChart(
+    person2.dateString,
+    person2.timeString,
+    person2.lat,
+    person2.lng,
+    person2.timezone
+  );
+  const person1NavamsaChart = getNavamsaChart(person1BirthChart);
+  const person2NavamsaChart = getNavamsaChart(person2BirthChart);
+  let total_score = 0;
+
+
+
+  return total_score >= threshold;
+}
 
 module.exports = { areCompatibile };
